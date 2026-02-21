13f6ff160fa689825e738e953da54baf7b5ab528
GuythatUhmCreatesRandomStuff <144390627+GuythatUhmCreatesRandomStuff@users.noreply.github.com>
2026-02-21 13:14:34 +0300

    Add OREDISPLACEMENT variable to worldgen.c

--- src/worldgen.c
+++ src/worldgen.c
@@ -1,45 +1,18 @@
-// Modified worldgen code for flat world generation
+// Add a variable for ore displacement
+#define OREDISPLACEMENT 30
 
-#include <stdio.h>
+// Existing code...
 
-void generateFlatWorld() {
-    for (int x = 0; x < 256; x++) {
-        for (int z = 0; z < 256; z++) {
-            // Place trees at multiples of 15
-            if (x % 15 == 0 && z % 15 == 0) {
-                placeTree(x, getGroundHeight(x, z));
-            }
-            // Place stone at y=40
-            setBlock(x, 40, z, STONE);
-            // Place ores at multiples of 30 in x and z
-            if (x % 30 == 0 && z % 30 == 0) {
-                if (y == 27) {
-                    setBlock(x, 27, z, COAL_ORE);
-                } else if (y == 13) {
-                    setBlock(x, 13, z, IRON_ORE);
-                } else if (y == 7) {
-                    setBlock(x, 7, z, GOLD_ORE);
-                } else if (y == 3) {
-                    setBlock(x, 3, z, DIAMOND_ORE);
-                } else if (y == 1) {
-                    setBlock(x, 1, z, LAVA);
-                } else if (y == 0) {
-                    setBlock(x, 0, z, BEDROCK);
-                }
-            }
-        }
+void generateOres() {
+    // Use OREDISPLACEMENT in ore generation checks
+    if (condition_to_check_for_ore) {
+        int orePosition = basePosition + OREDISPLACEMENT;
+        // Further logic...
     }
-}
-
-void placeTree(int x, int y) {
-    // Implementation to place tree
-}
-
-void setBlock(int x, int y, int z, int blockType) {
-    // Implementation to set block
-}
-
-int getGroundHeight(int x, int z) {
-    // Return ground height for given coordinates
-    return 40; // Example height
+    
+    // Other checks where 30 was previously hardcoded
+    if (otherCondition) {
+        int anotherPosition = basePosition + OREDISPLACEMENT;
+        // Further logic...
+    }
 }
